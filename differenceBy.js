c57b009e00b801495003fc4c762fae57a91634e7
shiva <readytohackk@gmail.com>
2019-02-20 18:06:17 +0530

    add differenceBy

---
+++ differenceBy.js
@@ -0,0 +1,7 @@
+Array.prototype.differenceBy = function(value, iteratee) {
+  let array = this.map(a => iteratee(a));
+  value = value.map(a => iteratee(a));
+  return array.filter(a => {
+    return value.indexOf(a) < 0;
+  });
+};
