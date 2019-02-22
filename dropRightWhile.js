115fd245d957c5153f170741cf997ac44265bbb1
shiva <readytohackk@gmail.com>
2019-02-22 20:09:31 +0530

    dropRightWhile changes

--- dropRightWhile.js
+++ dropRightWhile.js
@@ -2,8 +2,11 @@ Array.prototype.dropRightWhile = function(predicate) {
   let newArray = [];
   for (let i = this.length - 1; i >= 0; i--) {
     if (!predicate.apply(this, [this[i], i, this])) {
-      newArray.push(this[i]);
+      newArray = this.slice(0, i + 1);
+      break;
     }
   }
   return newArray;
 };
+
+///to do
